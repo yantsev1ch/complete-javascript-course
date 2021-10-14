e148287d551e2dd9c9fdfe6b273868fe87dcf952
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-14 23:09:02 +0300

    complete lesson about variables and values

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirsJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirsJob);
+
