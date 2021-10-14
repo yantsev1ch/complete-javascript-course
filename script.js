9db7ffca45136fe71970e9c643ade1dcfd6ef08d
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-15 01:34:47 +0300

    complete challenge #2

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ console.log(`String with
 multiple
 lines`);
 
- */
+
 const age = 15;
 
 if (age >= 18) {
@@ -167,3 +167,24 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+
+ */
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher!`)
+}
