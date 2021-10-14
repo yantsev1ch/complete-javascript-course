f4556ccb0fcd9f837eb878be9699b80b5b62be95
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-15 01:22:03 +0300

    complete lesson about if_else statements

--- script.js
+++ script.js
@@ -126,7 +126,6 @@ const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
 
- */
 
 const firstName = 'Jonas';
 const job = 'teacher';
@@ -148,3 +147,23 @@ lines');
 console.log(`String with
 multiple
 lines`);
+
+ */
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years 😊`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
