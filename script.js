535cf43b3c870bb60b59d897ee7cdd69f75fde23
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-15 16:16:44 +0300

    complete lesson about truthy and falsy values

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ if (BMIMark > BMIJohn) {
     console.log(`John's BMI (${BMIJohn}) is higher!`)
 }
 
- */
+
 
 // type conversion
 const inputYear = '1991';
@@ -210,3 +210,27 @@ console.log('23' > '18');
 let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
+
+ */
+
+//5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 1;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
