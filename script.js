75089100f569ae67061c23b937a40fb570c7940e
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-15 19:48:00 +0300

    complete lesson about switch statement

--- script.js
+++ script.js
@@ -284,7 +284,6 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
     console.log('Someone else should drive...');
 }
 
- */
 
 
 const averageDolphins = (96 + 108 + 89) / 3;
@@ -326,6 +325,34 @@ if (averageDolphinsBonus2 === averageKoalasBonus2 && teams >= 100) {
     console.log('Both win!')
 } else {
     console.log('No one wins')
+}
+
+ */
+
+
+const day = 'thursday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
 }
 
 
