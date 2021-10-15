1c01f89d2ae6161cb4060c81faf2de6b5d751443
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-15 16:33:44 +0300

    complete lesson about equality operators == vs. ===

--- script.js
+++ script.js
@@ -211,7 +211,7 @@ let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
 
- */
+
 
 //5 falsy values: 0, '', undefined, null, NaN
 
@@ -234,3 +234,28 @@ if (height) {
 } else {
     console.log('Height is UNDEFINED');
 }
+
+ */
+
+
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favourite = Number(prompt('What your favourite number?'));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number!')
+} else if (favourite === 7) {
+    console.log('7 is also a cool number')
+} else if (favourite === 9) {
+    console.log('9 is also a cool number')
+} else {
+    console.log('Number is not 23 or 7 or 9')
+}
+
+if (favourite !== 23) console.log('Why not 23?');
+
