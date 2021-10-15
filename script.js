1b90d8055899953fd74171ce0b065af7c3e1b54d
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-15 20:06:25 +0300

    complete lesson about conditional (Ternary) operator

--- script.js
+++ script.js
@@ -355,7 +355,7 @@ switch (day) {
         console.log('Not a valid day!');
 }
 
- */
+
 
 
 3 + 4
@@ -370,5 +370,23 @@ if (23 > 10) {
 const me = 'Jonas'
 console.log(`I'm ${2037 - 1991} years old ${me}`)
 
+
+ */
+
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine 🍷';
+} else {
+    drink2 = 'water 💧';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
 
 
