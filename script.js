ef8d12c02513494468fcd76c2c291419fc80f8c4
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-15 16:05:41 +0300

    complete lesson about conversion and coercion

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ if (birthYear <= 2000) {
 }
 console.log(century);
 
- */
+
 
 const massMark = 78;
 const heightMark = 1.69;
@@ -188,3 +188,25 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher!`)
 }
+
+ */
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+console.log('23' > '18');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
