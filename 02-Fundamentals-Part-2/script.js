c036f6b046c87621ee6fc7cd0f7bc3bf821c8837
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-16 02:03:00 +0300

    complete lesson about reviewing functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -67,7 +67,7 @@ const yearsUntilRetirement = (birthYeah, firstName) => {
 console.log(yearsUntilRetirement(1992, 'Andrei'));
 console.log(yearsUntilRetirement(1997, 'Vika'));
 
- */
+
 
 
 function cutFruitPieces(fruit) {
@@ -83,4 +83,25 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+
+ */
+
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah)
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`)
+        return -1;
+    }
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
 
