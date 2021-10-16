bfa3bed6304aec1e663684d7239b6da284941f5d
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-16 16:01:06 +0300

    complete challenge #2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -175,7 +175,7 @@ console.log(age1, age2, age3);
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
 
- */
+
 
 
 const friends = ['Michael', 'Steven', 'Peter'];
@@ -209,6 +209,20 @@ console.log(friends.includes(23));
 if (friends.includes('Peter')) {
     console.log('You have a friend called Peter');
 }
+
+
+ */
+
+
+const calcTip = function (bill) {
+   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
+
 
 
 
