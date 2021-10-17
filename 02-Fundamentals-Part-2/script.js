9f6315487d24abc7080f9f21921409d54d9d05a8
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-17 19:32:06 +0300

    complete lesson about iteration, for loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -315,7 +315,7 @@ console.log(jonas.age);
 // Challenge
 console.log(jonas.getSummary());
 
- */
+
 
 
 const mark = {
@@ -341,6 +341,25 @@ const john = {
 mark.calcBMI();
 john.calcBMI();
 console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`)
+
+ */
+
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
 
 
 
