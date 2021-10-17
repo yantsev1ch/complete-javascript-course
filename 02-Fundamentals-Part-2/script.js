1197b595b75982948f4951323054ffefbc04f3ed
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-18 02:31:04 +0300

    complete lesson about looping backwards and loops in loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -362,7 +362,7 @@ for (let rep = 1; rep <= 10; rep++) {
 }
 
 
- */
+
 
 
 
@@ -410,6 +410,30 @@ for (let i = 0; i < jonasArray.length; i++) {
     console.log(jonasArray[i], typeof jonasArray[i]);
 }
 
+
+ */
+
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--- Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+    }
+}
 
 
 
