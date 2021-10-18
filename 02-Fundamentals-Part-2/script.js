343980e9202b0af207277b48625cd93eb1323134
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-18 21:14:49 +0300

    complete challenge #4

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -435,7 +435,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
     }
 }
 
- */
+
 
 
 
@@ -457,6 +457,33 @@ while (dice !== 6) {
     dice = Math.trunc(Math.random() * 6) + 1;
     if (dice === 6) console.log('Loop is about to end');
 }
+
+ */
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bill.length; i++) {
+    const tip = calcTip(bill[i]);
+    tips.push(tip);
+    totals.push(tip + bill[i]);
+}
+console.log(bill, tips, totals);
+
+
+
+const calcAverage = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage([2,3,7]));
+
 
 
 
